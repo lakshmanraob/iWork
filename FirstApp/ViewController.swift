fb35b0457766ceb38d0c456f7ee636fc3d68c94d
Battula, Lakshmana Rao (US - Bengaluru) <labattula@deloitte.com>
2016-08-07 22:48:54 +0530

    god Question construction

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -12,23 +12,35 @@ class ViewController: UIViewController {
 
     var myName:String = ""
     var score:Int = 0;
+    var godName:String = ""
     
     @IBOutlet weak var firstLabel: UILabel!
     
     @IBOutlet weak var godLabel: UILabel!
     
+    @IBAction func LdKrishan(sender: AnyObject) {
+        greatGod()
+    }
+    @IBAction func LdChrist(sender: AnyObject) {
+        greatGod()
+    }
+    @IBAction func LdMohammad(sender: AnyObject) {
+        greatGod()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myName = "is great always"
-        print(myName)
-        firstLabel.text = "Jai Shri Ram! \(myName)"
-        self.firstMethod(10,iVal:10)
+        firstLabel.text = "1. Who is great God?"
+        //self.firstMethod(10,iVal:10)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func greatGod() {
+        godLabel.text = "Wrong Answer! All are equal."
     }
     
     func firstMethod(points:Int,iVal:Int){
