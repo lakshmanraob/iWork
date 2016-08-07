fbfd566df51cafca5cbb6b6398731125e6a1a128
Battula, Lakshmana Rao (US - Bengaluru) <labattula@deloitte.com>
2016-08-07 17:08:24 +0530

    myFirst Commit with two labels

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,14 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myName:String = ""
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var godLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myName = "is great always"
+        print(myName)
+        firstLabel.text = "Jai Shri Ram! \(myName)"
+        godLabel.text = "to show the power of God"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func firstMethod(){
+        let score:Int = 100
+        
+        print(score);
     }
 
 
