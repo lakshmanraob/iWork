6139839fcc622f6f00c3b2b66fdcefd60bb0c53a
Battula, Lakshmana Rao (US - Bengaluru) <labattula@deloitte.com>
2016-08-07 15:54:36 +0530

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Battula, Lakshmana Rao (US - Bengaluru) on 07/08/16.
+//  Copyright © 2016 Battula, Lakshmana Rao (US - Bengaluru). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
